551310577fa4f7c7aa926be8c52f8424e90f0902
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-09 16:16:39 +0900

    define method for donation controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,18 @@
 class DonationsController < ApplicationController
   def index
   end
+
+  def new
+    @donation = User.new
+  end
+
+  def create
+    @donation = User.new(donation_params)
+  end
+
+  private
+
+  def donation_params
+    params.require(:user).permit(:name, :name_reading, :nickname)
+  end
 end
