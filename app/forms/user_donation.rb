7a8bfe414cb9429cf0546532e50700d0a6f7c03e
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-11 16:06:21 +0900

    Revert "set save methos"
    This reverts commit 8c89a15628d8b0bf9600a43c27b690f4014d3f28.

--- app/forms/user_donation.rb
+++ app/forms/user_donation.rb
@@ -1,7 +1,7 @@
 class UserDonation
 
   include ActiveModel::Model
-
+  
   attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
 
 
@@ -20,14 +20,5 @@ class UserDonation
   validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
   # 「寄付金」の金額範囲に関するバリデーション
   validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is out of setting range"}
-
-  def save
-    # ユーザーの情報を保存し、「user」という変数に入れている
-    user = User.create(name: name, name_reading: name_reading, nickname: nickname)
-    # 住所の情報を保存
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
-    # 寄付金の情報を保存
-    Donation.create(price: price, user_id: user.id)
-  end
 
 end
