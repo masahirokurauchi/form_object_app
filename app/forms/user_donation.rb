ba19cc40afacc7e71ad62e35ce9f2bad74e4679a
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-11 15:47:14 +0900

    set validation of address and donation

--- app/forms/user_donation.rb
+++ app/forms/user_donation.rb
@@ -8,6 +8,16 @@ class UserDonation
     validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+
+    # 「住所」の郵便番号に関するバリデーション
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include include hyphen(-)"}
+    # 「寄付金」に関するバリデーション
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
   end
-  
+
+  # 「住所」の都道府県に関するバリデーション
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  # 「寄付金」の金額範囲に関するバリデーション
+  validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is out of setting range"}
+
 end
