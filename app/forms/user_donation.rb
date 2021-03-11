9306e8596cad1381a2d00699cae443fd38f8fd95
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-11 15:46:17 +0900

    move validation to form objects

--- app/forms/user_donation.rb
+++ app/forms/user_donation.rb
@@ -3,4 +3,11 @@ class UserDonation
   include ActiveModel::Model
   attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
 
+
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  end
+  
 end
